fbdbbe86bbcc8d4b152e80c994acb4782f2b9535
andy9705 <63641316+andy9705@users.noreply.github.com>
2022-11-16 02:27:28 +0900

    create 부분 기능 구현 안된 상태입니다( 구현한 기능은 이미지 등록하기 누르면 자기 갤러리에서 사진 가져오는 거만 일단 해놓았습니다.)

--- app/src/main/java/edu/skku/cs/groupbuying/ui/create/CreateFragment.java
+++ app/src/main/java/edu/skku/cs/groupbuying/ui/create/CreateFragment.java
@@ -1,21 +1,40 @@
 package edu.skku.cs.groupbuying.ui.create;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.content.Context;
+import android.content.ContentResolver;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
+import android.widget.DatePicker;
+import android.app.DatePickerDialog;
+import java.util.Calendar;
 import edu.skku.cs.groupbuying.MainActivity;
 import edu.skku.cs.groupbuying.R;
 import edu.skku.cs.groupbuying.databinding.FragmentCreateBinding;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class CreateFragment extends Fragment {
 
@@ -28,9 +47,60 @@ public class CreateFragment extends Fragment {
 
         binding = FragmentCreateBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        DatePickerDialog.OnDateSetListener callbackMethod;
+        final Uri[] uri = new Uri[1];
+        final Button selectImageBtn = binding.selectImageBtn;
+        final ImageView imageView = binding.imageView2;
+
+        final EditText title = binding.title;
+        final EditText maxPeople = binding.textView5;
+        final EditText setCalender = binding.textView9;
+        final EditText setLocation = binding.textView6;
+        final EditText detailExplanation = binding.textView7;
+        final Button finishButton = binding.create;
+
+
+
+
+
+
+        ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>(){
+                    @Override
+                    public void onActivityResult(ActivityResult result){
+                        if(result.getResultCode()==RESULT_OK && result.getData()!=null){
+                            uri[0] = result.getData().getData();
+                            try{
+
+                                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), uri[0]);
+                                imageView.setImageBitmap(bitmap);
+                            }catch (FileNotFoundException e){
+                                e.printStackTrace();
+                            }catch (IOException e){
+                                e.printStackTrace();
+                            }
+                        }
+
+                    }
+
+                }
+        );
+
+        selectImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityResult.launch(intent);
+            }
+        });
+
 
         return root;
     }
+
+
 
     @Override
     public void onDestroyView() {
