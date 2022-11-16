275ccdeb19e0f43e57e0c051caae4a6d10ab4241
2JaeSung <boy980705@gmail.com>
2022-11-17 02:08:01 +0900

    add create

--- app/src/main/java/edu/skku/cs/groupbuying/ui/create/CreateFragment.java
+++ app/src/main/java/edu/skku/cs/groupbuying/ui/create/CreateFragment.java
@@ -8,6 +8,8 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,28 +24,54 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import android.widget.DatePicker;
 import android.app.DatePickerDialog;
+import android.widget.Toast;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Calendar;
 import edu.skku.cs.groupbuying.MainActivity;
 import edu.skku.cs.groupbuying.R;
 import edu.skku.cs.groupbuying.databinding.FragmentCreateBinding;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 public class CreateFragment extends Fragment {
 
+    private int token;
     private FragmentCreateBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getArguments();
+        token = bundle.getInt("token");
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         CreateViewModel createViewModel =
                 new ViewModelProvider(this).get(CreateViewModel.class);
+
+        Bundle bundle = getArguments();
+        token = bundle.getInt("token");
+
 
         binding = FragmentCreateBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
@@ -55,9 +83,15 @@ public class CreateFragment extends Fragment {
         final EditText title = binding.title;
         final EditText maxPeople = binding.textView5;
         final EditText setCalender = binding.textView9;
-        final EditText setLocation = binding.textView6;
         final EditText detailExplanation = binding.textView7;
         final Button finishButton = binding.create;
+
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enroll(title, maxPeople, setCalender, detailExplanation);
+            }
+        });
 
 
 
@@ -106,6 +140,79 @@ public class CreateFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+
+    public void enroll(TextView title, TextView member, TextView date, TextView detail){
+        OkHttpClient client = new OkHttpClient();
+
+        List<Pair> params = new ArrayList<Pair>();
+        params.add(new Pair("title", title.getText().toString()));
+        params.add(new Pair("detail", detail.getText().toString()));
+        params.add(new Pair("targetMember", member.getText().toString()));
+        params.add(new Pair("dueDate", date.getText().toString()));
+        byte[] postData = CreateQuery(params, "UTF-8");
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("http://52.78.137.254:8080/content/new").newBuilder();
+        urlBuilder.addQueryParameter("token", Integer.toString(token));
+        String url = urlBuilder.build().toString();
+
+        Request req = new Request.Builder()
+                .url(url)
+                .post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"),postData))
+                .build();
+
+        client.newCall(req).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                final int code = response.code();
+
+                MainActivity activity = (MainActivity) getActivity();
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(code == 200){
+                            MainActivity activity = (MainActivity) getActivity();
+                            activity.CreateToHome(token);
+                            Toast toast=Toast.makeText(getContext(),"성공적으로 생성했습니다",Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                        else{
+                            Toast toast=Toast.makeText(getContext(),"생성에 실패했습니다",Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+
+    public static byte[] CreateQuery(List<Pair> pairs, String charset) {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        try {
+            for (Pair pair : pairs) {
+
+                if (first)
+                    first = false;
+                else
+                    result.append('&');
+
+                result.append(URLEncoder.encode((String) pair.first, charset));
+                result.append('=');
+                result.append(URLEncoder.encode((String) pair.second, charset));
+            }
+        }
+        catch( Exception e ) {
+
+        }
+        return result.toString().getBytes();
     }
 
 }
