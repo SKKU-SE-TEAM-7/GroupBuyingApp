93c60f13fc97db251d1e2e22f6f3183b471688c9
andy9705 <63641316+andy9705@users.noreply.github.com>
2022-11-19 03:08:04 +0900

    Update GlobalObject.java

--- app/src/main/java/edu/skku/cs/groupbuying/GlobalObject.java
+++ app/src/main/java/edu/skku/cs/groupbuying/GlobalObject.java
@@ -2,7 +2,12 @@ package edu.skku.cs.groupbuying;
 
 public class GlobalObject {
     private static int token;
+    private static String email;
 
     public static void setToken(int token) { GlobalObject.token = token; }
     public static int getToken() { return GlobalObject.token; }
+
+    public static void setEmail(String email){ GlobalObject.email=email;}
+    public static String getEmail() { return GlobalObject.email;}
+
 }
